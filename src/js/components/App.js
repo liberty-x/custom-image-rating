aa16ec38ac2955642d565d7c4c53f608cc26bfa9
Huw <hdrdavies@gmail.com>
2015-11-05 11:47:35 +0000

    image printing out on page

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -5,14 +5,20 @@ var App = React.createClass({
 
   getInitialState: function () {
     return {
-      items: []
+      fieldName: "HTML",
+      image: "http://pngimg.com/upload/star_PNG1580.png",
+      maxRating: 5,
+      rating: 3
     }
   },
 
   render: function () {
 
     var chartProps  = {
-      items: this.state.items
+      fieldName: this.state.HTML,
+      image: this.state.image,
+      maxRating: this.state.maxRating,
+      rating: this.state.rating
     };
 
     return(
--- src/js/components/Chart.js
+++ src/js/components/Chart.js
@@ -4,11 +4,18 @@ var Chart = React.createClass({
 
   render: function () {
 
-    // var items = this.props.items
+    var fieldName = this.props.fieldName;
+    var image     = this.props.image;
+    var maxRating = this.props.maxRating;
+    var rating    = this.props.rating;
 
     return (
       <div className="chart-container">
         <p>Hello chart</p>
+        <p>{fieldName}</p>
+        <img src={image}></img>
+        <p>{maxRating}</p>
+        <p>{rating}</p>
       </div>
     )
   }
